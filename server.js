3c813d9f7ba9110a221974a84e0992ddc7609058
Adam Jahur <adamnjahur@gmail.com>
2017-03-07 14:21:16 -0500

    added packages and server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const port = 5000;
+const bodyParser = require('body-parser');
+const api = require('./api/route/apiRoute');
+
+app.use(logger('common'));
+app.use(express.static(__dirname + "/app"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/api', api);
+
+app.listen(port, () => {
+    console.log("listening on http://localhost:%s", port);
+});
